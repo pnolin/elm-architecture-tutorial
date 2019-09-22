6a07f9554d6aab88190dd5683de407fd1272b77a
Patrick Nolin <patrick.nolin95@gmail.com>
2019-09-21 20:14:32 -0400

    Forms exercice.
    - More validation for password.
    - Age field.
    - Submit button.

--- examples/03-form.elm
+++ examples/03-form.elm
@@ -1,7 +1,9 @@
+module Main exposing (Model, Msg(..), init, main, update, view, viewInput, viewValidation)
+
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onClick, onInput)
 
 
 
@@ -9,7 +11,7 @@ import Html.Events exposing (onInput)
 
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
 
 
 
@@ -17,15 +19,17 @@ main =
 
 
 type alias Model =
-  { name : String
-  , password : String
-  , passwordAgain : String
-  }
+    { name : String
+    , age : String
+    , password : String
+    , passwordAgain : String
+    , submitted : Bool
+    }
 
 
 init : Model
 init =
-  Model "" "" ""
+    Model "" "" "" "" False
 
 
 
@@ -33,22 +37,30 @@ init =
 
 
 type Msg
-  = Name String
-  | Password String
-  | PasswordAgain String
+    = Name String
+    | Age String
+    | Password String
+    | PasswordAgain String
+    | Submit
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Name name ->
-      { model | name = name }
+    case msg of
+        Name name ->
+            { model | name = name }
 
-    Password password ->
-      { model | password = password }
+        Age age ->
+            { model | age = age }
 
-    PasswordAgain password ->
-      { model | passwordAgain = password }
+        Password password ->
+            { model | password = password }
+
+        PasswordAgain password ->
+            { model | passwordAgain = password }
+
+        Submit ->
+            { model | submitted = True }
 
 
 
@@ -57,22 +69,53 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ viewInput "text" "Name" model.name Name
-    , viewInput "password" "Password" model.password Password
-    , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
-    , viewValidation model
-    ]
+    div []
+        [ viewInput "text" "Name" model.name Name
+        , viewInput "text" "Age" model.age Age
+        , viewInput "password" "Password" model.password Password
+        , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+        , button [ onClick Submit ] [ text "Submit" ]
+        , viewValidation model
+        ]
 
 
 viewInput : String -> String -> String -> (String -> msg) -> Html msg
 viewInput t p v toMsg =
-  input [ type_ t, placeholder p, value v, onInput toMsg ] []
+    input [ type_ t, placeholder p, value v, onInput toMsg ] []
 
 
 viewValidation : Model -> Html msg
 viewValidation model =
-  if model.password == model.passwordAgain then
-    div [ style "color" "green" ] [ text "OK" ]
-  else
-    div [ style "color" "red" ] [ text "Passwords do not match!" ]
+    if not model.submitted then
+        div [ style "color" "green" ] [ text "Ok!" ]
+
+    else if String.toInt model.age == Nothing then
+        div [ style "color" "red" ] [ text "The age must be a number!" ]
+
+    else if String.length model.password <= 8 then
+        div [ style "color" "red" ] [ text "Passwords must have more than 8 characters!" ]
+
+    else if not <| passwordHasAllRequiredCharacterTypes model.password then
+        div [ style "color" "red" ] [ text "The password musth have at least one uppercase, lowercase and number!" ]
+
+    else if model.password /= model.passwordAgain then
+        div [ style "color" "red" ] [ text "Passwords do not match!" ]
+
+    else
+        div [ style "color" "green" ] [ text "Ok!" ]
+
+
+passwordHasAllRequiredCharacterTypes : String -> Bool
+passwordHasAllRequiredCharacterTypes password =
+    stringHasAtLeastOneCharacterType password Char.isLower
+        && stringHasAtLeastOneCharacterType password Char.isUpper
+        && stringHasAtLeastOneCharacterType password Char.isDigit
+
+
+stringHasAtLeastOneCharacterType : String -> (Char -> Bool) -> Bool
+stringHasAtLeastOneCharacterType string validation =
+    let
+        list =
+            String.toList string
+    in
+    List.any validation list
